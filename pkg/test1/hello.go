bc2e3aae77280e95b3ba514603b0bdcf1da6b8b3
tcxgo <2941999690@qq.com>
2022-02-17 14:46:55 +0800

    feat:添加撒盐

--- pkg/test1/hello.go
+++ pkg/test1/hello.go
@@ -1,5 +1,6 @@
 package test1
 import "fmt"
+import "github.com/mr-tcx/go-tmodule/internal/test2"
 type TestSay struct {
 	Name string
 }
@@ -10,4 +11,9 @@ func (ts *TestSay) TestSayHello() {
 
 func Say() {
 	fmt.Println("this is tmodule-test1")
+	test2.Say()
+}
+
+func Say2() {
+	fmt.Println("this is say2")
 }
